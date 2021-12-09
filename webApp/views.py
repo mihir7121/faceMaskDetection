287785800aa849c8eeb43f9d21bd0fd3231753eb
Ameya Ranade <62432771+Ameya1104@users.noreply.github.com>
2021-12-09 21:31:41 +0530

    render video

--- webApp/views.py
+++ webApp/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import datetime
+from django.http.response import StreamingHttpResponse
+import numpy as np
+import cv2
+from webApp.camera import VideoCamera
 
 def index(request):
     return render(request, 'webApp/index.html')
 
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def rendervideo(request):
+    return StreamingHttpResponse(gen(VideoCamera()), content_type = 'multipart/x-mixed-replace; boundary=frame;')
